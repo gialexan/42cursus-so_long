fd54ab61d510f4af9b8f974dc78747836ff6bfe3
gialexan | Gilmar Romani <gialexan@student.42sp.org.br>
2022-12-30 13:01:01 +0100

    remove includes

--- src_bonus/so_long_bonus.h
+++ src_bonus/so_long_bonus.h
@@ -6,7 +6,7 @@
 /*   By: gialexan <gialexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 19:19:16 by gialexan          #+#    #+#             */
-/*   Updated: 2022/12/30 12:45:56 by gialexan         ###   ########.fr       */
+/*   Updated: 2022/12/30 13:00:42 by gialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 //Includes
 # include "../minilibx/mlx.h"
 # include "../libft/libft.h"
-# include <stdlib.h>
 # include <fcntl.h>
 
 //Sprites Other
